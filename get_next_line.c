2115522c06d5f647f5a08e2f4480c3e635428d09
Velocitypulse <velocitypulse@outlook.fr>
2015-12-22 01:06:45 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/18 14:43:33 by cchameyr          #+#    #+#             */
-/*   Updated: 2015/12/22 01:04:03 by                  ###   ########.fr       */
+/*   Updated: 2015/12/22 01:05:55 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,8 +73,10 @@ static int		ft_capture(const int fd, char **line, char **end_chain)
 	if (end_chain != NULL)
 		ft_memdel((void *)end_chain);
 	if (capture[ft_checkline(capture) + 1])
+	{
 		end_chain = (char **)ft_strdup(&capture[ft_checkline(capture) + 1]);
-	end_chain[ft_strlen(&capture[ft_checkline(capture) + 1])] = 0;
+		end_chain[ft_strlen(&capture[ft_checkline(capture) + 1])] = 0;
+	}
 	*line = ft_strdup(ft_strsub(capture, 0, ft_checkline(capture)));
 	free(capture);
 	if (ret < BUFF_SIZE)
