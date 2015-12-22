0429ea2a30876ecb1f8977259917dcb094619f9e
Velocitypulse <velocitypulse@outlook.fr>
2015-12-22 16:03:18 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/18 14:43:33 by cchameyr          #+#    #+#             */
-/*   Updated: 2015/12/22 15:33:34 by                  ###   ########.fr       */
+/*   Updated: 2015/12/22 15:38:38 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,63 +53,11 @@ static	char	*ft_swapchain(char *s1, char *s2)
 
 static int		ft_capture(const int fd, char **line, char **end_chain)
 {
-	int		ret;
-	char	*buff;
-	char	*capture;
-	int		i;
 
-	ret = BUFF_SIZE;
-	capture = NULL;
-	buff = (char *)ft_memalloc(BUFF_SIZE + 1);
-	while (!(ft_strstr(buff, "\n")) && ret == BUFF_SIZE)
-	{
-		ft_bzero(buff, BUFF_SIZE);
-		if ((ret = read(fd, buff, BUFF_SIZE)) == -1)
-			return (-1);
-		capture = ft_swapchain(capture, buff);
-	}
-	ft_memdel((void **)&buff);
-	if (end_chain != NULL)
-		ft_memdel((void *)end_chain);
-	if (capture[(i = ft_checkline(capture)) + 1])
-	{
-		*end_chain = ft_strdup(&capture[i + 1]);
-		end_chain[0][ft_strlen(&capture[i]) - 1] = 0;
-	}
-	*line = ft_strsub(capture, 0, i);
-	ft_memdel((void **)&capture);
-	if (ft_checkline(*end_chain) == ft_strlen(*end_chain))
-		return(0);
-	return (1);
 }
+
 
 int		get_next_line(const int fd, char **line)
 {
 	static char	*end_chain = NULL;
-	char		*temp;
-	int			i;
-	int			state;
-
-	if (end_chain == NULL)
-		return (ft_capture(fd, line, &end_chain));
-	else
-	{
-		if ((i = ft_checkline(end_chain)) < ft_strlen(end_chain))
-		{
-			YOLO
-			ft_memdel((void **)line);
-			*line = ft_strsub(end_chain, 0, i);
-			end_chain = &end_chain[i];
-			return (1);
-		}
-		else
-		{
-			ft_memdel((void **)line);
-			temp = ft_strdup(end_chain);
-			temp[ft_strlen(end_chain)] = 0;
-			state = ft_capture(fd, line, &end_chain);
-			*line = ft_swapchain(temp, *line);
-			return (state);
-		}
-	}
 }
